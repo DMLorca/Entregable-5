09a963e4de0e1055122eadfd2ebdf62b54be4e49
DMLorca <105229809+DMLorca@users.noreply.github.com>
2022-05-25 23:30:03 -0300

    Add files via upload

---
+++ Entregable_pug/entregable-5-pug.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+
+let modulo = require('./Contenedor.js');
+let contenedor = new modulo.Contenedor('productos.txt');
+
+app.use(express.urlencoded({extended: true}));
+
+let producto = [];
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+    res.render('form', {producto});
+});
+
+app.get('/productos', (req, res) => {
+    contenedor.getAll().then((result) => {
+        producto = result;
+        res.render('vista', {producto});
+    })
+});
+
+app.post('/productos', (req, res) => {
+    const {title, price, thumbnail} = req.body;
+    
+    const obj = {
+        'title': title,
+        'price': price,
+        'thumbnail': thumbnail
+    }
+
+    async function ejecutarSave(argObj){
+        await contenedor.save(argObj);
+        }
+    ejecutarSave(obj); 
+
+    res.redirect('/');
+});
+
+app.listen(8080, () => {
+    console.log('Servidor levantado');
+});
